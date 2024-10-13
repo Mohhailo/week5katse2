74086d1d49ad78b45eac8c42e2b4e22020017702
Mõhhailo Riško <misha@woodman.ee>
2024-10-13 19:59:11 +0300

    add movie cord

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+//select movie poster original image
+let moviePoster = document.querySelector('.movie-poster');
+//select model windov layer
+let modal = document.querySelector('#myModal');
+//select close button
+let closeBtn = document.querySelector('.closeBtn');
+//select modal window image
+let modalImage = document.querySelector('.movie-poster-modal');
+
+
+moviePoster.addEventListener('click',()=> {
+console.log('image cliced');
+modal.style.display = 'block';
+modalImage.src = moviePoster.src;
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';   
+} );
+
+window.addEventListener('click', (event) => {
+if(event.target === modal){
+    modal.style.display = 'none'  
+}
+} );
+
+
+
